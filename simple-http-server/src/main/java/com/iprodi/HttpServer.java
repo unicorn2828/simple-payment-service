c8e7bd42d15eacee812530d79a2a6d8f312e3127
unicorn2828 <260v260@gmail.com>
2025-11-17 18:53:17 +0300

    edit response again on master branch

--- simple-http-server/src/main/java/com/iprodi/HttpServer.java
+++ simple-http-server/src/main/java/com/iprodi/HttpServer.java
@@ -27,7 +27,10 @@ public class HttpServer {
         }
 
         // создание ответа
-        String response = "<h1>Hello from http server</h1>";
+        String response = """
+        <h1>Hello from http server</h1>
+        <h2>It is working</h2>
+        """;
         out.write("HTTP/1.1 404 OK\r\n");
         out.write("Content-Type: text/html; charset=UTF-8\r\n");
         out.write("Content-Length: " + response.length() + "\r\n");
